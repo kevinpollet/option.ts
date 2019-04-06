55b386fb185010162eb01645375c093a9dcb2d9b
kevinpollet <pollet.kevin@gmail.com>
2019-04-06 11:10:03 +0400

    Remove old files

--- src/hello.ts
+++
@@ -1,8 +0,0 @@
-/**
- * Copyright © 2019 kevinpollet <pollet.kevin@gmail.com>`
- *
- * Use of this source code is governed by an MIT-style license that can be
- * found in the LICENSE.md file.
- */
-
-export const hello = (name: string): string => `Hello ${name}`;
